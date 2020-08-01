180545a4a799ed0cf6082aba9560cf1bb8a6c2ed
joma74 <dev-mails@gmx.net>
2020-08-01 22:18:12 +0200

    feat: login and logout against keycloak running on http://localhost:9112

---
+++ src/client/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+var oauth = struct {
+	authURL   string
+	logoutURL string
+}{
+	authURL:   "http://localhost:9112/auth/realms/myrealm/protocol/openid-connect/auth",
+	logoutURL: "http://localhost:9112/auth/realms/myrealm/protocol/openid-connect/logout",
+}
+
+var t = template.Must(template.ParseFiles("template/index.html"))
+
+var authCodeVars = struct {
+	code         string
+	sessionState string
+}{}
+
+func main() {
+	fmt.Println("Server starting")
+	http.HandleFunc("/", home)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/authCodeRedirect", authCodeRedirect)
+	http.HandleFunc("/logout", logout)
+	http.ListenAndServe(":9110", nil)
+}
+
+func home(rs http.ResponseWriter, rq *http.Request) {
+	t.Execute(rs, nil)
+}
+
+func login(rs http.ResponseWriter, rq *http.Request) {
+	nrq, nerr := http.NewRequest("GET", oauth.authURL, nil)
+	if nerr != nil {
+		log.Print(nerr)
+		return
+	}
+	qs := url.Values{}
+	qs.Add("client_id", "oauth-nailed-app-1")
+	qs.Add("response_type", "code")
+	qs.Add("state", "123")
+	qs.Add("redirect_uri", "http://localhost:9110/authCodeRedirect")
+	nrq.URL.RawQuery = qs.Encode()
+	http.Redirect(rs, rq, nrq.URL.String(), http.StatusFound)
+}
+
+/**
+* Location: http://localhost:9110/authCodeRedirect?state=123&session_state=6c634b86-8a30-...beaf&code=a16dcfbc-d53b-...-a66dbcfac9c1
+ */
+func authCodeRedirect(rs http.ResponseWriter, rq *http.Request) {
+	fmt.Printf("Request query: %v", rq.URL.Query())
+	fmt.Println()
+	authCodeVars.code = rq.URL.Query().Get("code")
+	authCodeVars.sessionState = rq.URL.Query().Get("session_state")
+	http.Redirect(rs, rq, "/", http.StatusFound)
+}
+
+func logout(rs http.ResponseWriter, rq *http.Request) {
+	nrq, nerr := http.NewRequest("GET", oauth.logoutURL, nil)
+	if nerr != nil {
+		log.Print(nerr)
+		return
+	}
+	qs := url.Values{}
+	qs.Add("redirect_uri", "http://localhost:9110/")
+	nrq.URL.RawQuery = qs.Encode()
+	http.Redirect(rs, rq, nrq.URL.String(), http.StatusFound)
+}

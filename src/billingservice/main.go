22990ee2e6a28ff347b9c6094f16a9050c6bcfb7
joma74 <dev-mails@gmx.net>
2020-08-20 00:58:16 +0200

    feat: Added JWT introspection with keycloak

--- src/billingservice/main.go
+++ src/billingservice/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 	"reflect"
 	"runtime"
+	"strings"
 
 	"learn.oauth.billingservice/model"
 )
@@ -14,6 +17,14 @@ var oauthServer = struct {
 	introspectionURL string
 }{
 	introspectionURL: "http://localhost:9112/auth/realms/myrealm/protocol/openid-connect/token/introspect",
+}
+
+var oauthClient = struct {
+	clientID       string
+	clientPassword string
+}{
+	clientID:       "oauth-nailed-app-1-token-checker",
+	clientPassword: "7e9247c4-bcbe-4783-89f0-880d83ac147f",
 }
 
 func main() {
@@ -50,8 +61,11 @@ func services(rs http.ResponseWriter, rq *http.Request) {
 	fmt.Println("Access token provided:", accessToken)
 	//
 	if !isAccessTokenValid(accessToken) {
-
+		fmt.Println("Access token is not valid or not active")
+		rs.WriteHeader(http.StatusForbidden)
+		return
 	}
+	fmt.Println("Access token is valid and active")
 	//
 	s := model.BillingServicesResponse{Services: []string{"electric", "phone", "internet", "water"}}
 	rs.Header().Add("Content-Type", "application/json")
@@ -63,7 +77,11 @@ func getAccessToken(rq *http.Request) (string, error) {
 	// On Authorization Request Header Field, see https://tools.ietf.org/html/rfc6750#section-2.1
 	accessToken := rq.Header.Get("Authorization")
 	if accessToken != "" {
-		return accessToken, nil
+		authorizationHeaderParts := strings.Split(accessToken, " ")
+		if len(authorizationHeaderParts) != 2 {
+			return accessToken, fmt.Errorf("Authorization header format is invalid")
+		}
+		return authorizationHeaderParts[1], nil
 	}
 	// On Form-Encoded Body Parameter, see https://tools.ietf.org/html/rfc6750#section-2.2
 	accessToken = rq.FormValue("access_token")
@@ -79,5 +97,39 @@ func getAccessToken(rq *http.Request) (string, error) {
 }
 
 func isAccessTokenValid(accessToken string) bool {
-	return true
+	form := url.Values{}
+	form.Add("token", accessToken)
+	form.Add("token_type_hint", "requesting_party_token")
+	nrq, nerr := http.NewRequest("POST", oauthServer.introspectionURL, strings.NewReader(form.Encode()))
+	nrq.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	nrq.SetBasicAuth(oauthClient.clientID, oauthClient.clientPassword)
+	if nerr != nil {
+		fmt.Print("Could not prepare request", nerr)
+		return false
+	}
+	c := http.Client{}
+	nrs, nerr := c.Do(nrq)
+	if nerr != nil {
+		fmt.Println("Could not send introspection request", nerr)
+		return false
+	}
+	if nrs.StatusCode != 200 {
+		fmt.Println("Introspection response status is expected to be 200 but was ", nrs.StatusCode)
+		return false
+	}
+	byteBody, nerr := ioutil.ReadAll(nrs.Body)
+	defer nrs.Body.Close()
+	if nerr != nil {
+		fmt.Println("Could not read introspection response body", nerr)
+		return false
+	}
+
+	introspectionRequestingPartyTokenResponse := &model.IntrospectionRequestingPartyTokenResponse{}
+	nerr = json.Unmarshal(byteBody, introspectionRequestingPartyTokenResponse)
+	if nerr != nil {
+		fmt.Println("Could not unmarshal response to model.IntrospectionRequestingPartyTokenResponse", nerr)
+		return false
+	}
+
+	return introspectionRequestingPartyTokenResponse.Active
 }
---
+++ src/billingservice/model/IntrospectionRequestingPartyTokenResponse.go
@@ -0,0 +1,13 @@
+package model
+
+// IntrospectionRequestingPartyTokenResponse Response of token introspection in the form was requested as token_type_hint with value requesting_party_token
+type IntrospectionRequestingPartyTokenResponse struct {
+	Exp    int    `json:"exp"`
+	Nbf    int    `json:"nbf"`
+	Iat    int    `json:"iat"`
+	Jti    string `json:"jti"`
+	Aud    string `json:"aud"`
+	Typ    string `json:"typ"`
+	Acr    string `json:"acr"`
+	Active bool   `json:"active"`
+}

22497f2b9ecc39d957ce446cd37931ffeebf6da1
joma74 <dev-mails@gmx.net>
2020-08-25 01:05:26 +0200

    feat: Added react-router and memorize state from redirect

--- src/implicit-client/src/App.js
+++ src/implicit-client/src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import "./App.css"
+import AuthCodeRedirect from "./AuthCodeRedirect"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			access_token: "",
+			expires_in: "",
+			session_state: "",
+			token_type: "",
+		}
+	}
+
+	/**
+	 *
+	 * @param {string} k
+	 * @param {string} v
+	 */
+	_setStateValue = (k, v) => {
+		if (this.state[k] !== v) {
+			this.setState({ [k]: v })
+		}
+	}
+
+	render() {
+		return (
+			<Router>
+				<div>
+					<nav>
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/login">Login</Link>
+							</li>
+							<li>
+								<Link to="/services">Services</Link>
+							</li>
+							<li>
+								<Link to="/logout">Logout</Link>
+							</li>
+						</ul>
+					</nav>
+
+					{/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+					<Switch>
+						<Route path="/login">
+							<Login />
+						</Route>
+						<Route path="/services">
+							<Services />
+						</Route>
+						<Route
+							path="/authCodeRedirect"
+							render={() => (
+								<AuthCodeRedirect _setStateValue={this._setStateValue} />
+							)}
+						></Route>
+						<Route path="/logout">
+							<Logout />
+						</Route>
+						<Route path="/">
+							<Home />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
+		)
+	}
 }
 
-export default App;
+function Login() {
+	window.location.href =
+		"http://localhost:9112/auth/realms/myrealm/protocol/openid-connect/auth?client_id=oauth-nailed-app-2-implicit-grant&response_type=token&redirect_uri=http://localhost:3000/authCodeRedirect"
+	return null
+}
+
+function Services() {
+	return <h2>Services</h2>
+}
+
+function Logout() {
+	return <h2>Logout</h2>
+}
+
+function Home() {
+	return <h2>Home</h2>
+}
+
+export default App
---
+++ src/implicit-client/src/AuthCodeRedirect.js
@@ -0,0 +1,34 @@
+import { Component } from "react"
+import { withRouter } from "react-router-dom"
+
+class AuthCodeRedirect extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+
+	static getDerivedStateFromProps({ _setStateValue, location }, state) {
+		// get access token
+		const hashURL = location.hash
+		const hashURLAsMap = hashURL
+			.substr(1)
+			.split("&")
+			.reduce((acc, item) => {
+				const kv = item.split("=")
+				const k = kv[0]
+				const v = kv[1]
+				acc[k] = v
+				return acc
+			}, {})
+		_setStateValue("access_token", hashURLAsMap["access_token"])
+		_setStateValue("expires_in", hashURLAsMap["expires_in"])
+		_setStateValue("session_state", hashURLAsMap["session_state"])
+		_setStateValue("token_type", hashURLAsMap["token_type"])
+		return null
+	}
+	render() {
+		return null
+	}
+}
+
+export default withRouter(AuthCodeRedirect)
